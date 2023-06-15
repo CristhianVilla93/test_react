37b0c9fdce522941458fd7f3c6b2c94c682df437
Cristhian Villa <crvm1993@gmail.com>
2023-06-14 21:49:03 -0500

    tercer comentario

--- src/hoteles/Hotel.jsx
+++ src/hoteles/Hotel.jsx
@@ -15,16 +15,27 @@ export default function Hotel() {
 
             <div className="buscador">
               <input className="pais" type="text" />
-              <div  className="personas">
-                <input type="text" />
-                <button></button>
+              <div className="personas">
+                <input className="cant-per" type="text" />
+                <button className="botoncito"></button>
               </div>
             </div>
           </div>
           <div className="contenedor-sec2">
-            <h1 className="titulo-conte">Stays in Finland</h1>
+            <div className="tituprim">
+              <h1 className="titulo-conte">Stays in Finland</h1>
+              <span>12+ stays</span>
+            </div>
             <div className="contenedor-cart">
               <Cartelera />
+              <Cartelera />
+              <Cartelera />
+              <Cartelera />
+              <Cartelera />
+              <Cartelera />
+              <Cartelera />
+              <Cartelera />
+              
              
             </div>
           </div>
